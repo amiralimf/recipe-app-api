769c12d8081d4426c4dd98ff1720f70a23ae6323
amirali <198871527+amiralimf@users.noreply.github.com>
2025-05-10 17:14:09 +0330

    Build recipe API

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -36,3 +37,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # for login with email :D
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """ Recipe object """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
